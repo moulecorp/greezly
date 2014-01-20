132e1fd6b334e5ebb3df86d8f056f820ca3f6ea5
Antoine Tenart <atenart@n0.pe>
2014-01-21 00:04:13 +0100

    Add merge_theirs.sh script

---
+++ scripts/greezly/merge_theirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+from=$1
+to=$2
+
+git checkout $1
+git merge -s ours --no-edit $2
+git branch tmpMerge
+git reset --hard $2
+git reset --soft tmpMerge
+git checkout $1 scripts/greezly
+git commit --amend --no-edit
+git branch -D tmpMerge
