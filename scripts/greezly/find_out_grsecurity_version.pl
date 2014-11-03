94688a48b9b848817324eb9e2d4bebd613db3b1c
Antoine Ténart <atenart@n0.pe>
2014-11-03 09:10:54 +0100

    update script to find out grsec version
    Signed-off-by: Antoine Ténart <atenart@n0.pe>

--- scripts/greezly/find_out_grsecurity_version.pl
+++ scripts/greezly/find_out_grsecurity_version.pl
@@ -10,5 +10,5 @@ my $mech = WWW::Mechanize->new ();
 $mech->get ('http://grsecurity.net/download.php');
 die "Connexion error" unless $mech->success ();
 
-my $link = $mech->find_link (text_regex => qr/\.patch$/, n => 1);
+my $link = $mech->find_link (text_regex => qr/\.patch$/, n => 2);
 print $link->text ();
