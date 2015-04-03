48a7cb16502c39e7a8adc65229ecfce19a756dfb
Moule Corp <greezly@moulecorp.org>
2015-04-03 09:33:23 +0200

    Revert "Yet again, update the grsec version script"
    This reverts commit 2daf4229533cc8fbaa58da991b44e524e0419392.

--- scripts/greezly/find_out_grsecurity_version.pl
+++ scripts/greezly/find_out_grsecurity_version.pl
@@ -10,5 +10,5 @@ my $mech = WWW::Mechanize->new ();
 $mech->get ('http://grsecurity.net/download.php');
 die "Connexion error" unless $mech->success ();
 
-my $link = $mech->find_link (text_regex => qr/\.patch$/, n => 2);
+my $link = $mech->find_link (text_regex => qr/\.patch$/, n => 1);
 print $link->text ();
